c7e7a126603eed094f73ba3a8160f9b3acc2f197
JD Kwo <JD Kwo@DESKTOP-INAAQE7.dhcp.wustl.edu>
2024-04-10 17:22:57 -0500

    we tried. we coded. we succeeded.

--- src/studio9/Polynomial.java
+++ src/studio9/Polynomial.java
@@ -3,96 +3,113 @@ package studio9;
 import java.util.LinkedList;
 
 public class Polynomial {
-	
+
 	private LinkedList<Double> list;
 
 	/**
 	 * Constructs a Polynomial with no terms yet.
 	 */
 	public Polynomial() {
-		//FIXME
+		list = new LinkedList<>();
 	}
 
-	
 	/**
 	 * 
 	 * @param coeff
 	 * @return polynomial with added term
 	 */
 	public void addTerm(double coeff) {
-		//FIXME
+		list.add(coeff);
 	}
-	
+
 	/*
 	 * Returns a String of the polynomial with the proper form:
 	 * 
 	 * Cx^N + Cx^N-1 + ... + Cx + C
 	 */
 	public String toString() {
-		return ""; //FIXME
+		//return ""; 
+		int degree = list.size()-1;
+		String mt = "";
+		for (double coeff: list) {
+			if(degree>0)
+				mt += coeff + "X^" + degree+"+";
+			else
+				mt += coeff;
+			degree-=1;
+		}
+		return mt;
+
 	}
-	
+
 	/**
 	 * 
 	 * @param x
 	 * @return value of polynomial at that x
 	 */
 	public double evaluate(double x) {
-		return 0;//FIXME
+		int degree = list.size()-1;
+		double coeff2 = 0;
+		for(double coeff: list) {
+			coeff2 += coeff*Math.pow(x,degree);
+			degree --;
+		}
+		return coeff2;
 	}
 
-	
 	public Polynomial derivative() {
-		return null;//FIXME
+		Polynomial derivative = new Polynomial(); 
+			int degree = list.size()-1;
+			double newCoeff = 0;
+			for(int i=0; i< degree; ++i) {
+			newCoeff = (degree-1)*list.get(i); 
+			derivative.addTerm(newCoeff);
+		}
+		return derivative;
 	}
-	
 
 	/**
-	 * This is the "equals" method that is called by
-	 *    assertEquals(...) from your JUnit test code.
-	 *    It must be prepared to compare this Polynomial
-	 *    with any other kind of Object (obj).  Eclipse
-	 *    automatically generated the code for this method,
-	 *    after I told it to use the contained list as the basis
-	 *    of equality testing.  I have annotated the code to show
-	 *    what is going on.
+	 * This is the "equals" method that is called by assertEquals(...) from your
+	 * JUnit test code. It must be prepared to compare this Polynomial with any
+	 * other kind of Object (obj). Eclipse automatically generated the code for this
+	 * method, after I told it to use the contained list as the basis of equality
+	 * testing. I have annotated the code to show what is going on.
 	 */
 
 	public boolean equals(Object obj) {
 		// If the two objects are exactly the same object,
-		//    we are required to return true.  The == operator
-		//    tests whether they are exactly the same object.
+		// we are required to return true. The == operator
+		// tests whether they are exactly the same object.
 		if (this == obj)
 			return true;
 		// "this" object cannot be null (or we would have
-		//    experienced a null-pointer exception by now), but
-		//    obj can be null.  We are required to say the two
-		//    objects are not the same if obj is null.
+		// experienced a null-pointer exception by now), but
+		// obj can be null. We are required to say the two
+		// objects are not the same if obj is null.
 		if (obj == null)
 			return false;
 
-		//  The two objects must be Polynomials (or better) to
-		//     allow meaningful comparison.
+		// The two objects must be Polynomials (or better) to
+		// allow meaningful comparison.
 		if (!(obj instanceof Polynomial))
 			return false;
 
 		// View the obj reference now as the Polynomial we know
-		//   it to be.  This works even if obj is a BetterPolynomial.
+		// it to be. This works even if obj is a BetterPolynomial.
 		Polynomial other = (Polynomial) obj;
 
 		//
 		// If we get here, we have two Polynomial objects,
-		//   this and other,
-		//   and neither is null.  Eclipse generated code
-		//   to make sure that the Polynomial's list references
-		//   are non-null, but we can prove they are not null
-		//   because the constructor sets them to some object.
-		//   I cleaned up that code to make this read better.
-
+		// this and other,
+		// and neither is null. Eclipse generated code
+		// to make sure that the Polynomial's list references
+		// are non-null, but we can prove they are not null
+		// because the constructor sets them to some object.
+		// I cleaned up that code to make this read better.
 
 		// A LinkedList object is programmed to compare itself
-		//   against any other LinkedList object by checking
-		//   that the elements in each list agree.
+		// against any other LinkedList object by checking
+		// that the elements in each list agree.
 
 		return this.list.equals(other.list);
 	}
